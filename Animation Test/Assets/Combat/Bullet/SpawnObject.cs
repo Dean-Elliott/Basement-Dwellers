b97e867f8bc0c3619b578466b270e22d5c97a180
Shellmadinger <48625443+Shellmadinger@users.noreply.github.com>
2020-01-28 17:34:32 -0500

    Added a shoot mechanic for player

---
+++ Animation Test/Assets/Combat/Bullet/SpawnObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+    //Initializing Variables
+    public GameObject asset;
+    public Transform spawnPoint;
+
+
+    // Use this for initialization
+    void Update()
+    {
+        //Instantiate Game Object
+        if (Input.GetButton("Fire1"))
+        {
+            Instantiate(asset, spawnPoint.position, Quaternion.identity);
+        }
+       
+    }
+ 
+}
