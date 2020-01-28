a092bc069dff2bc2540e613b17cb9dc791281a5b
Shellmadinger <48625443+Shellmadinger@users.noreply.github.com>
2020-01-28 15:27:05 -0500

    Added a health script

---
+++ Animation Test/Assets/character/Scripts/Health.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float health;
+    public float healthMax;
+    public float damage;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckHealth();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            health -= damage;
+        }
+    }
+
+    void CheckHealth()
+    {
+        if (health > healthMax)
+        {
+            health = healthMax;
+        }
+
+        if (health < 0)
+        {
+            health = 0;
+        }
+    }
+}
