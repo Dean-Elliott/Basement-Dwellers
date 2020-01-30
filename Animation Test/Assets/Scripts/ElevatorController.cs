fbdf0678ef9ff2823523fe7cf110a3524dde0695
DrewGodden <35704776+DrewGodden@users.noreply.github.com>
2020-01-30 03:30:55 -0500

    Implement elevator states and basic functions
    Implemented elevator controller with elevator states and basic functionality that moves the elevator upwards when the player steps on it.

---
+++ Animation Test/Assets/Scripts/ElevatorController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorController : MonoBehaviour
+{
+    public enum ElevatorStates
+    {
+        Resting = 0,
+        GoingUp = 1,
+        GoingDown = 2
+    }
+    ElevatorStates elevatorState;
+
+    public GameObject elevatorFloor;
+
+    public float speed;
+
+    public GameObject[] waypoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (elevatorState)
+        {
+            case ElevatorStates.Resting:
+                //Elevator is resting and awaiting input
+                break;
+
+            case ElevatorStates.GoingUp:
+                GoUp();
+                break;
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            elevatorState = ElevatorStates.GoingUp;
+        }
+    }
+
+    private void GoUp()
+    {
+        if (elevatorFloor.transform.position.y < waypoints[1].transform.position.y)
+        {
+            elevatorFloor.transform.position += elevatorFloor.transform.up * speed * Time.deltaTime;
+        }
+    }
+}
