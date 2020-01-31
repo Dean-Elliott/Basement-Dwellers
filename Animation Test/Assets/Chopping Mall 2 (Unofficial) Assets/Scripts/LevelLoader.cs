2c8804efab703616cf51abeb90dcd33fa91426dc
David D'Orazio <daviddorazio8@gmail.com>
2020-01-30 23:24:59 -0500

    Implement Scene Transition Code
    Added the code so now the scene transition will fade in when a scene is opened

---
+++ Animation Test/Assets/Chopping Mall 2 (Unofficial) Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionLoadDelay = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            LoadNextLevel(); 
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionLoadDelay);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
+
