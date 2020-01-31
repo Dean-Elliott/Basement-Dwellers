dd67b38407deb21129279aa350c7ae58efb2be1d
DrewGodden <35704776+DrewGodden@users.noreply.github.com>
2020-01-30 23:22:51 -0500

    Add paint can objects and metal hit sounds
    Added paint can rigidbody objects to Drew's Test Scene and four metal hit sounds to be added as impact sound effects in a later commit.

---
+++ Animation Test/Assets/PaintCanController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintCanController : MonoBehaviour
+{
+    public AudioClip[] metalHitSounds;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
