5df1baf72795d5930d38a1c3223ebd8b45257856
David D'Orazio <daviddorazio8@gmail.com>
2020-01-30 21:59:31 -0500

    Implement Lock And Key Script
    I wrote the script for a lock and key system. when playyer picks up key then goes over to the lock and presses e. it unlocks the door

---
+++ Animation Test/Assets/Chopping Mall 2 (Unofficial) Assets/Scripts/KeyCollection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCollection : MonoBehaviour
+{
+    public bool hasKey = false;
+    public GameObject key;
+    public GameObject door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hasKey == false)
+        {
+            key.SetActive(true);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Key")
+        {
+            hasKey = true;
+            key.SetActive(false);
+        }
+
+        if (other.gameObject.tag == "Lock" && hasKey == true && Input.GetButtonDown("E"))
+        {
+            door.SetActive(false);
+        }
+    }
+}
