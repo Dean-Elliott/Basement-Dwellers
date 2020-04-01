f851b0cb2e95bb8d13fa7dc4d80fd6ac78128970
Shellmadinger <48625443+Shellmadinger@users.noreply.github.com>
2020-04-01 17:13:28 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/Dean-Elliott/Basement-Dwellers""
    This reverts commit 8b5cb6ceddb43806d03c83500b4f3bc35de3ac6d.

---
+++ Animation Test/Assets/TrackAnalytics.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class TrackAnalytics : MonoBehaviour
+{
+    private bool isFinishedSendingResults;
+    private float timeSpentInLevelBeforeDeath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeSpentInLevelBeforeDeath = 0;
+        isFinishedSendingResults = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFinishedSendingResults == false)
+        {
+            timeSpentInLevelBeforeDeath += Time.deltaTime;
+        }
+
+        if (Health.isPlayerDead == true && isFinishedSendingResults == false)
+        {
+            ReportAnalytics();
+        }
+    }
+
+    public void ReportAnalytics()
+    {
+        AnalyticsResult waveNumberResult = Analytics.CustomEvent("Wave number before death", new Dictionary<string, object>
+        {
+            { "Wave number", EnemySpawnerController.currentWave }
+        });
+        Debug.Log("Wave number before death " + waveNumberResult);
+
+        AnalyticsResult jumpsMadeResult = Analytics.CustomEvent("Jumps made before death", new Dictionary<string, object>
+        {
+            { "Jumps made", PlayerMovementV2.jumpsMade }
+        });
+        Debug.Log("Jumps made before death " + jumpsMadeResult);
+
+        AnalyticsResult shotsMadeResult = Analytics.CustomEvent("Shots made before death", new Dictionary<string, object>
+        {
+            { "Shots made", SpawnObject.shots }
+        });
+        Debug.Log("Shots made before death " + shotsMadeResult);
+
+        AnalyticsResult enemiesKilledResult = Analytics.CustomEvent("Enemies killed before death", new Dictionary<string, object>
+        {
+            { "Enemies killed", TestEnemyController.enemiesKilled }
+        });
+        Debug.Log("Enemies killed before death " + enemiesKilledResult);
+
+        AnalyticsResult timeSpentInLevelResult = Analytics.CustomEvent("Time spent in level before death",
+            new Dictionary<string, object>
+        {
+            { "Time spent in level", timeSpentInLevelBeforeDeath }
+        });
+        Debug.Log("Time spent in level before death " + timeSpentInLevelResult);
+
+        isFinishedSendingResults = true;
+    }
+}
